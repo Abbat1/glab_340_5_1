2aa4c0889db376cff0a4faee299c479b95608e66
Abbat1 <cozzyyist@gmail.com>
2024-10-07 11:02:28 -0500

    lab_for_operators

---
+++ python_operator.py
@@ -0,0 +1,93 @@
+a=32  #initiliaze the value of a
+b=6  #initiliaze the value of b
+
+#Below is how we use basic operators and we print them in the terminal.
+
+# print('Addition of two numbers:',a+b)  
+# print('Subtraction of two numbers:',a-b)  
+# print('Multiplication of two numbers:',a*b)  
+# print('Division of two numbers:',a/b)  
+# print('Reminder of two numbers:',a%b)  
+# print('Exponent of two numbers:',a**b)  
+# print('Floor division of two numbers:',a//b)  
+# MultipleOperators = 1 + 2 * 3 / 4.0
+# print("Result of MultipleOperators is: ",MultipleOperators)
+
+
+# Below is how we use Assignment operators and we print them in the terminal.
+
+
+# a = 32         # Initialize the value of a  
+# b = 6          # Initialize the value of b  
+# print('a=b:', a==b)  #
+# print('a+=b:', a+b)  # shorthand of a = a + b
+# print('a-=b:', a-b)  # shorthand of a = a - b
+# print('a*=b:', a*b)  # shorthand of a = a * b
+# print('a%=b:', a%b)  # shorthand of a = a % b
+# print('a**=b:', a**b)  # shorthand of a = a ** b
+# print('a//=b:', a//b)  # shorthand of a = a/ b  
+   
+# j = 10 # j gets the value 10.
+# print("j is : " , j)
+# j = 5 # j gets the value 5. The previous value is overwritten.
+# k = j # k gets the value 5.
+# print("j is : " , j)
+# print("k is : " , k)
+
+
+# k = j = 10; # (k = (j = 10))
+# print("j is : " , j)
+# print("k is : " , k)
+
+
+# Below is an example code of Comparison operators thatwe can use in python.
+
+# a = 32      # Initialize the value of a  
+# b = 6       # Initialize the value of b  
+# print('Two numbers are equal or not:',a==b)  
+# print('Two numbers are not equal or not:',a!=b)  
+# print('a is less than or equal to b:',a<=b)  
+# print('a is greater than or equal to b:',a>=b)  
+# print('a is greater b:',a>b)  
+# print('a is less than b:',a<b) 
+
+# Below is an example code of Logical operators that we can use in python.
+
+# a=5              #initiliaze the value of a      
+# print('Is this statement true?:',a>3 and a<5)
+# print('Is this statement true?:',a>3 or a<5)
+# print('Is this statement true?:',(not(a>3 and a<5)))
+
+
+# #Below is an example code of Identity operators that we can use in python.
+
+
+# a=["rose", "lotus"]
+# b=["rose", "lotus"]
+# c=a
+# print(a is c)
+# print(a is not c)
+# print(a is b)
+# print(a is not b)
+# print(a == b)
+# print(a != b)
+
+
+
+#Below is an example code of Membership operators that we can use in python.
+
+x = ["rose", "lotus"]
+print('Is value Present?' "rose" in x) 
+print('Is value not Present?' "riya" not in x)
+
+x ='hello world'
+y ={1:'a',2:'b'}
+
+#check if 'h' is present in the string
+print('h' in x)
+#check in 'hello' is not present in the string
+print('hello' not in x)  # prints True
+# check if '1' key is present in y
+print(1 in y)  # prints True
+# check if 'a' key is present in y
+print('a' in y)  # prints False
