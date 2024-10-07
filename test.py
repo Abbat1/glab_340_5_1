a4e4114b5e71b9f3be1a37e6905c691f1ea74c8b
Abbat1 <cozzyyist@gmail.com>
2024-10-07 09:47:07 -0500

    first

---
+++ test.py
@@ -0,0 +1,10 @@
+#Belwo is the code to take input from user and display it back.
+#it is asking the name and age of the user and displaying it back.
+
+
+name = input("What's your name? ")
+age = input("Boi, how old you ought to be? ")
+
+print ("Hello," + name + "!")
+print ("run along now, you " + age + "years old .")
+
